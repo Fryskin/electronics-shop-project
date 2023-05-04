6e5cbbc288a02d5fb467df9ad22da418f6a55cb4
Fryskin <gman358325@gmail.com>
2023-05-04 09:00:52 +0200

    DZ_4.

--- src/phone.py
+++ src/phone.py
@@ -5,7 +5,7 @@ class Phone(Item):
 
     def __init__(self, item_name, price, quantity, number_of_sim: int):
         super().__init__(item_name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self._number_of_sim = number_of_sim
 
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
@@ -20,5 +20,15 @@ class Phone(Item):
         else:
             return None
 
+    @property
+    def number_of_sim(self):
+        return self._number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value):
+        if value <= 0:
+            raise ValueError
+        else:
+            self._number_of_sim = value
 
 
